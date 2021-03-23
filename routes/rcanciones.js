2f09cc725636aba0369b5c00ac9b4c1d3647999c
Alba <albaaparicio12@gmail.com>
2021-03-24 00:07:19 +0100

    SDI-2021-102-7.5-Vistas y motores de plantillas

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,7 +1,35 @@
-module.exports = function(app) {
+module.exports = function(app, swig) {
+
     app.get("/canciones", function(req, res) {
-        let respuesta = 'Nombre: ' + req.query.nombre + '<br>'
-            + 'Autor: '+ req.query.autor;
+        let canciones = [ {
+            "nombre" : "Blank space",
+            "precio" : "1.2"
+        },{
+            "nombre" : "See you again",
+            "precio" : "1.3"
+        },{
+            "nombre" : "Safaera",
+            "precio" : "1.1"
+        }];
+
+        let respuesta = swig.renderFile('views/btienda.html', {
+            vendedor : 'Tienda de canciones',
+            canciones : canciones
+        });
+
+        res.send(respuesta);
+    });
+
+    app.post('/cancion', function(req, res) {
+        res.send("Cancion agreagda:"+req.body.nombre + "<br>"
+        + " genero :" + req.body.genero + "<br>" +
+        " precio : "+ req.body.precio);
+    });
+
+    app.get('/canciones/agregar', function (req, res) {
+        let respuesta = swig.renderFile('views/bagregar.html', {
+
+        });
         res.send(respuesta);
     });
 
@@ -16,9 +44,5 @@ module.exports = function(app) {
         res.send(respuesta);
     });
 
-    app.post('/cancion', function(req, res) {
-        res.send("Cancion agreagda:"+req.body.nombre + "<br>"
-        + " genero :" + req.body.genero + "<br>" +
-        " precio : "+ req.body.precio);
-    });
+
 };
